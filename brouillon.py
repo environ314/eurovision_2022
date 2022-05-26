56ab259812030a0f272042ce1141100b2debbb3e
dawild <environ314@gmail.com>
2022-05-26 17:06:30 +0200

    EU comming

--- brouillon.py
+++ brouillon.py
@@ -41,4 +41,4 @@ resu=sorted(votes_recus["Ukraine"].items(),key = lambda t : t[1])
 for pays,points in resu:
     print(f'Pays : {pays}.\t Nb de pts : {points} points')
 
-print("version de 15:46")
+print("version de 16:56 bordel")
